79c01fae29a4ef13769dcfeedaba9d1b299db772
Robert Greathouse <greathouse.robert@gmail.com>
2013-04-25 21:59:45 -0500

    Add random select for bug pair (if even number of developers)

--- pairs/src/pairs/Main.scala
+++ pairs/src/pairs/Main.scala
@@ -5,11 +5,11 @@ import scala.collection.mutable.ListBuffer
 
 object Main {
 	def main(args: Array[String]) {
-	  val lines = scala.io.Source.fromFile(args(0), "utf-8").getLines
-	  val shuffled = Random.shuffle(lines)
+	  val lines = scala.io.Source.fromFile(args(0), "utf-8").getLines.toSeq 
+	  val shuffled = new Random(System.currentTimeMillis()).shuffle(lines)
 	  
 	  def printPairs = (a: Pair) => {
-		  Thread.sleep(1000)
+		  Thread.sleep(5000)
 		  println()
 		  print(a.dev1)
 		  Thread.sleep(1000)
@@ -21,9 +21,22 @@ object Main {
 		  print(if(a.dev2==null) "Gibby" else a.dev2)
 	  } 
 	  
-	  val paired = assignPairs(shuffled)
+	  val paired = assignPairs(shuffled.iterator)
+	  println("Thinking......")
 	  paired.foreach(printPairs)
-	  println(" (BUGS)")
+	  if (lines.length % 2 == 1) {
+	    println(" (BUGS)")
+	  }
+	  else {
+	    Thread.sleep(3500)
+	    println("\n\n...And the winning team on BUGS is......")
+	    Thread.sleep(5000)
+	    val rand = new Random(System.currentTimeMillis());
+	    val random_index = rand.nextInt(paired.length);
+	    val result = paired(random_index);
+	    println(result.dev1.toUpperCase()+" & "+result.dev2.toUpperCase())
+	    println("\nCONGRATULATIONS!!!!!")
+	  }
 	}
 	
 	def assignPairs(devs: Iterator[String]) : List[Pair] = {
